e60303b372d2c731740a7f50acebc75e9d0d7b08
Rizky Putra Mahendra <rizky.mahendra@amartek.id>
2025-10-09 13:55:32 +0700

    feat: initialize inventory project with React, Vite, Tailwind CSS, Shadcn/UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, aut minima
+        qui eum esse dignissimos minus totam inventore voluptatem iste, odio
+        nostrum repellat culpa. Soluta voluptatibus quam delectus ad tempora!
+      </p>
+    </main>
+  );
+}
+
+export default App;
