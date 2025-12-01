6ae0ff4eb6e8cc7d886e373eb734d1225b33d129
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-12-01 10:49:14 +0700

    fix: update product page header and remove user name display

--- src/pages/Authenticated/Products/index.tsx
+++ src/pages/Authenticated/Products/index.tsx
@@ -1,21 +1,17 @@
 import SectionWrapper from '@/components/SectionWrapper';
 import { useState } from 'react';
 import { TypographyH3 } from '@/components/ui/typography';
-import { type IUserAuth } from '@/types';
 
-import _ from 'lodash';
 import ProductTable from '@/components/ProductTable';
 import AddProductDialog from '@/components/ProductTable/AddProductDialog';
 
 const ProductsPage = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const user: IUserAuth = JSON.parse(localStorage.getItem('user') || 'null');
-
   return (
     <>
       <SectionWrapper>
-        <TypographyH3>Hello, {_.startCase(user.full_name)}</TypographyH3>
+        <TypographyH3>List of product</TypographyH3>
 
         <ProductTable setIsDialogOpen={setIsDialogOpen} />
       </SectionWrapper>
